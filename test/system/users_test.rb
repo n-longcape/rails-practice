373508b841354ce096a047a360f416f739d3ac9b
Nagasaki Naoto <kdkrniyuni@gmail.com>
2019-05-10 16:17:27 +0900

    14章終了まで

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -42,4 +42,15 @@ class UsersTest < ApplicationSystemTestCase
 
     assert_text "User was successfully destroyed"
   end
+
+  test "should follow and unfollow a user" do
+    michael = users(:michael)
+    archer  = users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    assert archer.followers.include?(michael)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
+  end
 end
